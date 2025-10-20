33dc61b7c130dd4632750878027f4d1dcf398785
Masahiro Yamazaki <masahiro777.ymzk@gmail.com>
2025-10-20 19:21:23 +0900

    Sulfuras, Hand of Ragnaros の場合だけ処理を分ける

--- gilded_rose/gilded_rose_ruby/gilded_rose.rb
+++ gilded_rose/gilded_rose_ruby/gilded_rose.rb
@@ -6,11 +6,11 @@ class GildedRose
 
   def update_quality
     @items.each do |item|
+      break if item.name == "Sulfuras, Hand of Ragnaros"
+      
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
+          item.quality = item.quality - 1
         end
       else
         if item.quality < 50
@@ -29,9 +29,9 @@ class GildedRose
           end
         end
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
+
+      item.sell_in = item.sell_in - 1
+
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
